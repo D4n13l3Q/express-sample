52b5d27b1b4739671b786c3ea642ce62d81666e9
Daniele Barbaro <barbaro.daniele@gmail.com>
2022-05-24 17:49:46 +0200

    Auth Middleware

--- app.js
+++ app.js
@@ -1,26 +1,31 @@
 import express from "express";
 import 'dotenv/config';
 import logMiddleware from "./middlewares/log.middleware.js";
+import authMiddleware from "./middlewares/auth.middleware.js";
 
 const port = process.env.PORT
 const app = express()
 
 app.use(express.json())
-app.use(logMiddleware);
+// app.use(logMiddleware); // <--
+// app.use(authMiddleware); //<---
 
-app.get('/',  (request, response) =>
+app.get('/', logMiddleware, (request, response) =>
     response.send('Sei nel progetto forkato')
 )
 
-app.get('/test', (request, response) => {
-    response
-        .status(200)
-        .json({
-            'data': 'Valore',
-            'status': 'Ok',
-            'value': 1,
-        })
-})
+app.get(
+    '/test',
+    [authMiddleware, logMiddleware],
+    (request, response) => {
+        response
+            .status(200)
+            .json({
+                'data': 'Valore',
+                'status': 'Ok',
+                'value': 1,
+            })
+    })
 
 app.all('*', (request, response) => {
     console.error('sei in una risorsa non corretta.')

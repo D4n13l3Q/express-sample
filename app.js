dcbbb5aedf5918a75227df6ddcd462a2956448c4
Daniele Barbaro <barbaro.daniele@gmail.com>
2022-05-24 17:23:56 +0200

    Lesson fixes

--- app.js
+++ app.js
@@ -1,10 +1,34 @@
 import express from "express";
 import 'dotenv/config';
 
+const port = process.env.port
 const app = express()
 
+app.use(express.json())
+
 app.get('/',  (request, response) =>
-    response.send('Hello World')
+    response.send('Sei nel progetto forkato')
 )
 
-app.listen(process.env.PORT || 3000)
+app.get('/test', (request, response) => {
+    response
+        .status(200)
+        .json({
+            'data': 'Valore',
+            'status': 'Ok',
+            'value': 1,
+        })
+})
+
+app.all('*', (request, response) => {
+    console.error('sei in una risorsa non corretta.')
+    response
+        .status(500)
+        .json({
+            'success': false,
+            'code': 1001,
+            'message': "Risorsa non disponibile"
+        })
+})
+
+app.listen(port || 3000)
--- routes/admin.route.js
+++ routes/admin.route.js
@@ -1,9 +1,9 @@
 import express from "express";
 
-const adminRoutes = express.Router();
+const adminRoute = express.Router();
 
-adminRoutes.get('/',
+adminRoute.get('/',
     (request, response) => response.send('Hello admin')
 )
 
-export default adminRoutes;
+export default adminRoute;
